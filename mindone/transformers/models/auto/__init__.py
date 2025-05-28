de845e7a4b3ab502aeb6edaa03189b5e8da70014
Yang <chenyang214@huawei.com>
2025-05-28 14:52:50 +0800

    2 push

--- mindone/transformers/models/auto/__init__.py
+++ mindone/transformers/models/auto/__init__.py
@@ -1,3 +1,3 @@
-from .configuration_auto import AutoConfig
+from .configuration_auto import AutoConfig,CONFIG_MAPPING
 from .image_processing_auto import AutoImageProcessor
 from .modeling_auto import AutoModel, AutoModelForCausalLM, AutoModelForMaskedLM
