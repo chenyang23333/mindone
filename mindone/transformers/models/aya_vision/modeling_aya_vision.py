961ae6d286316fd5be3dc47fa2d6cb1ce602686d
Yang <chenyang214@huawei.com>
2025-05-29 16:50:44 +0800

    2 push

--- mindone/transformers/models/aya_vision/modeling_aya_vision.py
+++ mindone/transformers/models/aya_vision/modeling_aya_vision.py
@@ -19,7 +19,7 @@ from mindone.transformers.modeling_utils import PreTrainedModel
 from mindone.transformers.processing_utils import Unpack
 from transformers.utils import LossKwargs, auto_docstring, can_return_tuple, is_torchdynamo_compiling
 from mindone.transformers.models.auto import AutoModel
-from .configuration_aya_vision import AyaVisionConfig
+from configuration_aya_vision import AyaVisionConfig
 
 class AyaVisionMultiModalProjector(nn.cell):
     def __init__(self, config: AyaVisionConfig):
