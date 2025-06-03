b8d2fe32883c157a55def7f1a4fe4e001db7c9ad
Yang <chenyang214@huawei.com>
2025-06-03 18:10:35 +0800

    comment _list_model_options

--- mindone/transformers/models/auto/configuration_auto.py
+++ mindone/transformers/models/auto/configuration_auto.py
@@ -45,7 +45,8 @@ CONFIG_MAPPING_NAMES = OrderedDict(
         ("qwen2", "Qwen2Config"),
         ("t5", "T5Config"),
         ("xlm-roberta", "XLMRobertaConfig"),
-        ("siglip_vision_model","SiglipVisionConfig")
+        ("siglip_vision_model","SiglipVisionConfig"),
+        ("cohere2", "Cohere2Config"),
     ]
 )
 
